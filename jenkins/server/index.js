3ca4c530831a17c323d6f1934bf7b51ea5da2035
Rachappa <newrachappabiradar786@gmail.com>
2026-03-14 01:38:08 +0530

    update server message

--- jenkins/server/index.js
+++ jenkins/server/index.js
@@ -3,9 +3,10 @@ const app = express();
 
 const VERSION = "v3";
 
-app.get("/", (req,res)=>{
-    res.send("CI/CD Demo  my name is rachapp: " + VERSION);
+app.get("/", (req, res) => {
+  res.send("New version deployed from Jenkins CI/CD");
 });
+
 
 app.listen(3000,()=>{
     console.log("Server running on port 3000");
